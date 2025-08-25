04eba403b730a1bf64271ad252b684830fcd8ce2
AllanSagastegui <xpheri035@gmail.com>
2025-08-25 18:20:17 -0500

    feat: configuración de metodos para el Swagger

--- infrastructure/entry-points/reactive-web/src/main/java/pe/com/ask/api/LoanApplicationHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/pe/com/ask/api/LoanApplicationHandler.java
@@ -1,10 +1,12 @@
 package pe.com.ask.api;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import pe.com.ask.api.dto.request.CreateLoanApplicationDTO;
+import pe.com.ask.api.dto.response.ResponseCreateLoanApplication;
 import pe.com.ask.api.exception.model.UnexpectedException;
 import pe.com.ask.api.exception.service.ValidationService;
 import pe.com.ask.api.mapper.LoanApplicationMapper;
@@ -40,5 +42,9 @@ public class LoanApplicationHandler {
                 ));
     }
 
-    public void createLoanApplicationDoc(CreateLoanApplicationDTO dto) {}
+    public Mono<ResponseCreateLoanApplication> createLoanApplicationDoc(
+            @RequestBody(description = "Loan Application - Data required to create a new loan application")
+            CreateLoanApplicationDTO dto) {
+        return Mono.empty();
+    }
 }
